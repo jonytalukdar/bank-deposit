ac3f97cebd92b2e6ae1edaed1ac7b9c9bdf0720a
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-26 12:37:47 +0600

    completed deposit section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// login button event handler
+
 let loginArea = document.getElementById('login-area');
 let transactionArea = document.getElementById('transaction-area');
 const loginButton = document.getElementById('login');
@@ -5,4 +7,18 @@ const loginButton = document.getElementById('login');
 loginButton.addEventListener('click', function () {
   loginArea.style.display = 'none';
   transactionArea.style.display = 'block';
+});
+
+// deposit button
+let depositButton = document.getElementById('deposit');
+depositButton.addEventListener('click', function () {
+  let depositAmount = document.getElementById('deposit-amount').value;
+  let depositNumber = parseFloat(depositAmount);
+
+  let currentDeposit = document.getElementById('current-deposit').innerText;
+  let currentDepositNumber = parseFloat(currentDeposit);
+  let totatDeposit = depositNumber + currentDepositNumber;
+
+  document.getElementById('current-deposit').innerText = totatDeposit;
+  document.getElementById('deposit-amount').value = '';
 });
