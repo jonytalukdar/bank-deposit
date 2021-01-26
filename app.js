fe659e1b34b0c34e4799d5da1ebb44d79911f5e2
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-26 14:54:55 +0600

    added one function

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ loginButton.addEventListener('click', function () {
 // deposit button
 let depositButton = document.getElementById('deposit');
 depositButton.addEventListener('click', function () {
-  let depositAmount = document.getElementById('deposit-amount').value;
-  let depositNumber = parseFloat(depositAmount);
+  const depositNumber = getInputNumber('deposit-amount');
 
   updateSpanText('current-deposit', depositNumber);
   updateSpanText('current-balance', depositNumber);
@@ -27,3 +26,17 @@ function updateSpanText(id, depositNumber) {
   let totatAmount = depositNumber + currentNumber;
   document.getElementById(id).innerText = totatAmount;
 }
+
+function getInputNumber(id) {
+  const amount = document.getElementById(id).value;
+  const withdrawNumber = parseFloat(amount);
+  return withdrawNumber;
+}
+
+// withDraw button event handler
+
+const withdrawButton = document.getElementById('withdraw');
+withdrawButton.addEventListener('click', function () {
+  const withdrawNumber = getInputNumber('withdraw-amount');
+  console.log(withdrawNumber);
+});
